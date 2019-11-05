53ab8a96cdd8692aca0a3b608da5b006b67a9a8b
wangjiang <541478324@qq.com>
2019-11-05 14:41:48 +0800

    drools入门案例第二次提交

--- src/main/java/com/cn/drool/test/DrollTest.java
+++ src/main/java/com/cn/drool/test/DrollTest.java
@@ -34,17 +34,26 @@ import java.util.Collection;
 public class DrollTest {
 
     public static void main(String[] args) {
+        /**
+         * KnowledgeBuilder:收集已编写好的规则，然后进行编译，产生一批编译好的规则包。
+         */
         KnowledgeBuilder builder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+        /*
+         * ResourceType：指定规则文件的类型。
+         * ResourceFactory：可从classPath,URL,File,ByteArray,Reader加载规则
+         */
         builder.add(ResourceFactory.newClassPathResource("rules/rule.drl",DrollTest.class), ResourceType.DRL);
         Collection<KiePackage> packages = builder.getKnowledgePackages();
         KieBaseConfiguration kbConfig = KnowledgeBaseFactory.newKnowledgeBaseConfiguration();
         kbConfig.setProperty("org.drools.sequential","true");
+        //产生规则包的集合
         InternalKnowledgeBase knowledgeBase = KnowledgeBaseFactory.newKnowledgeBase(kbConfig);
         knowledgeBase.addPackages(packages);
         KieSession session = knowledgeBase.newKieSession();
         Customer customer = new Customer();
         customer.setName("zhangsan");
         customer.setAge(30);
+//        session.update();
         session.insert(customer);
         session.fireAllRules();
         session.dispose();
